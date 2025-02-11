c87e05a89ecee1dc40ec0223ffd01208cfd7e816
__init__ <aavendanoca@unal.edu.co>
2025-02-11 04:18:00 +0100

    Added linked list class

---
+++ linked_list.h
@@ -0,0 +1,109 @@
+//
+// Created by 0srma on 4/02/2025.
+//
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+template <typename T>
+class Node {
+public:
+    T data;
+    Node* next;
+    Node* previous;
+    Node(T d) : data(d), next(nullptr), previous(nullptr) {}
+};
+
+template <typename T>
+class LinkedList {
+    public:
+    LinkedList() : head(nullptr), tail(nullptr) {}
+
+    public:
+    Node<T>* prepend(T value) {
+        Node<T>* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+            return newNode;
+        }
+        newNode->next = head;
+        head->previous = newNode;
+        head = newNode;
+        return newNode;
+    };
+
+    Node<T>* append(T value) {
+        if (head == nullptr) {
+            return prepend(value);
+        }
+        Node<T>* newNode = new Node<T>(value);
+        Node<T>* currentNode = head;
+        while (currentNode->next != nullptr) {
+            currentNode = currentNode->next;
+        }
+        currentNode->next = newNode;
+        newNode->previous = currentNode;
+        return newNode;
+    }
+
+    Node<T>* search(T value) const {
+        Node<T>* currentNode = head;
+        while (currentNode != nullptr) {
+            if (currentNode->data == value) {
+                return currentNode;
+            }
+            currentNode = currentNode->next;
+        }
+        return nullptr;
+    }
+
+    bool deleteNode(Node<T>* node) {
+        if (node == nullptr) return false;
+
+        if (node == head) {
+            head = head->next;
+            if (head != nullptr) {
+                head->previous = nullptr;
+            } else {
+                tail = nullptr; // List is now empty
+            }
+        } else if (node == tail) {
+            tail = tail->previous;
+            if (tail != nullptr) {
+                tail->next = nullptr;
+            } else {
+                head = nullptr; // List is now empty
+            }
+        } else {
+            Node<T>* previousNode = node->previous;
+            Node<T>* nextNode = node->next;
+            if (previousNode != nullptr) {
+                previousNode->next = nextNode;
+            }
+            if (nextNode != nullptr) {
+                nextNode->previous = previousNode;
+            }
+        }
+
+        delete node;
+        return true;
+    }
+
+    void printList() {
+        Node<T>* current = head;
+        while (current != nullptr) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+
+
+private:
+    Node<T>* head;
+    Node<T>* tail;
+};
+
+
+#endif //LINKED_LIST_H
--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#include <bitset>
+#include <iostream>
+#include <cstdint>
+
+#include "linked_list.h"
 
 
 int main() {
